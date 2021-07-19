de543daa1362a043676a99567fb582e115761694
unknown <cam.eudier@gmail.com>
2021-07-19 14:37:15 +0200

    ajout des RegEx sur formulaire de contact

--- JS/confirmation.js
+++ JS/confirmation.js
@@ -1,10 +1,20 @@
-const confirmationInfo = JSON.parse(localStorage.getItem("clientContact"));
+
+function getUrlParameter(name) {
+    // va récupérer paramètres de l'url
+    const parameters = new URLSearchParams(window.location.search);
+    // récupère le nom du paramètre
+    return parameters.get(name);
+}
+
+const orderId = getUrlParameter("orderId");
+const firstName = getUrlParameter("firstName");
+const totalPrice = getUrlParameter("totalPrice");
 
 const clientName = document.getElementById("clientName");
-clientName.innerHTML = teddy.firstName;
+clientName.innerHTML = firstName;
 
 const orderNumber = document.getElementById("orderNumber");
-orderNumber.innerHTML = "?? Numéro ??" ;
+orderNumber.innerHTML = orderId;
 
 const orderTotal = document.getElementById("orderTotal")
-orderTotal.innerHTML = teddy.totalToPay;
+orderTotal.innerHTML = totalPrice / 100 + " €";
--- JS/panier.js
+++ JS/panier.js
@@ -1,10 +1,14 @@
 
 const cartContent = JSON.parse(localStorage.getItem("cart"));
+let totalPrice = 0;
+const itemsId = [];
 
 for (const teddy of cartContent) {
+    totalPrice += teddy.price;
+    itemsId.push(teddy.id);
 
-    const newArticle = document.createElement("tr"); 
-    newArticle.classList.add("newArticle"); 
+    const newArticle = document.createElement("tr");
+    newArticle.classList.add("newArticle");
 
     const newName = document.createElement("td");
     newName.classList.add("newName");
@@ -16,61 +20,44 @@ for (const teddy of cartContent) {
 
     const newPrice = document.createElement("td");
     newPrice.classList.add("newPrice");
-    newPrice.innerHTML = teddy.price/100 + " €";
-
-    /* const numberOfArticles = document.createElement("td");
-    numberOfArticles.classList.add("numberOfArticles");
-    numberOfArticles.innerHTML = cartContent.number;
-
-    const newTotal = document.createElement("td");
-    newTotal.classList.add("newTotal");
-    newTotal.innerHTML = newPrice * numberOfArticles; */
+    newPrice.innerHTML = teddy.price / 100 + " €";
 
     listOfArticles.appendChild(newArticle);
     newArticle.appendChild(newName);
     newArticle.appendChild(newColor);
     newArticle.appendChild(newPrice);
-    /*newArticle.appendChild(numberOfArticles);
-    newArticle.appendChild(newTotal)
-*/
 
 }
 
-/* Total des articles 
-
-const TotalToPay = document.getElementById("basketTotalAmount");
-let sumPrice = 0;
-let addPrice = 0;
-addPrice = teddy.newTotal;
-sumPrice =  teddy.newTotal.lenght + addPrice;
-TotalToPay.innerHTML = sumPrice+" €"; */
+document.getElementById("basketTotalAmount").innerHTML = totalPrice / 100 + " €";
 
 
-////* Validation du formulaire et ouverture de la page confirmation *////
 
-document.getElementById("validationForm").addEventListener("submit" , function(e){
+/// Validation du formulaire et ouverture de la page confirmation  ///
+
+document.getElementById("validationForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    var erreur ;
+    var erreur;
     var name = document.getElementById("formName");
     var firstName = document.getElementById("formFirstName");
     var address = document.getElementById("formAddress");
     var city = document.getElementById("formCity");
     var email = document.getElementById("formEmail");
 
-    if (!name.value) {
+    if (!name.value || !validName(name.value)) {
         erreur = "Veuillez renseigner votre nom.";
     }
-    if (!firstName.value) {
+    if (!firstName.value || !validFirstName(firstName.value)) {
         erreur = "Veuillez renseigner votre prénom.";
     }
-    if (!address.value) {
+    if (!address.value || !validAddress(address.value)) {
         erreur = "Veuillez renseigner votre adresse.";
     }
-    if (!city.value) {
+    if (!city.value || !validCity(city.value)) {
         erreur = "Veuillez renseigner votre ville.";
     }
-    if (!email.value) {
+    if (!email.value || !validEmail(email.value)) {
         erreur = "Veuillez renseigner votre email.";
     }
 
@@ -79,39 +66,66 @@ document.getElementById("validationForm").addEventListener("submit" , function(e
         document.getElementById("formErreur").innerHTML = erreur;
         return false
 
-    } else {        
-        const clientData ={
+    } else {
+        const clientData = {
             contact: {
                 firstName: firstName.value,
                 lastName: name.value,
                 address: address.value,
                 city: city.value,
-                email: email.value 
-            }
+                email: email.value
+            },
+            products: itemsId
         }
 
-        let clientContact = localStorage.setItem("clientContact" , clientData);
-        if (clientContact) {
-            const contact = JSON.parse(clientContact);
-            addToLocalStorage(contact, clientData);
-        }
-        else {
-            const contact = [];
-            addToLocalStorage(contact, clientData);
-        }
-        
-        //addToLocalStorage(totalToPay) ??// ajout du montant total à payer
-        window.location.href = "confirmation.html"; // ouverture de la page confirmation.
-       
-        
+        fetch("http://localhost:3000/api/teddies/order", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(clientData)
+        }).then(res => {
+            if (res.ok) {
+                res.json().then(data => {
+                    localStorage.clear();
+                    window.location.href = "confirmation.html?orderId=" + data.orderId + "&firstName=" + data.contact.firstName + "&totalPrice=" + totalPrice; // ouverture de la page confirmation.
+                });
+            }
+        });
     }
 })
 
 function addToLocalStorage(contact, clientData) {
     contact.push(clientData);
     const clientDataString = JSON.stringify(contact);
-    localStorage.setItem("clientContact" , clientDataString);
+    localStorage.setItem("clientContact", clientDataString);
 }
 
+// Validation des données du formaulaire //
 
+function validName(name) {
+    const nameRegEx = /^[^0-9]+[a-zA-Z]+$/;
+    return nameRegEx.test(name);
+}
+
+function validFirstName(firstName) {
+    const firstNameRegEx = /^[^0-9]+[a-zA-Z]+$/;
+    return firstNameRegEx.test(firstName);
+}
+
+function validAddress(address) {
+    const addressRegEx = /^[0-9]+[a-zA-Z\u0080-\u024F\s\/\-\)\(\`\.\"\']+$/;
+    return addressRegEx.test(address);
+}
+
+function validCity(city) {
+    const cityRegEx = /^[a-zA-Z\u0080-\u024F\s\/\-\)\(\`\.\"\']+$/;
+    return cityRegEx.test(city);
+}
+
+function validEmail(email) {
+    const emailRegEx = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+    return emailRegEx.test(email);
+}
 
